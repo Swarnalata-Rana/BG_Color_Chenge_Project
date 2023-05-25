e1657b37040d6f012f8d837bb55f71168121defa
Swarnalata Rana <swarnalata21@navgurukul.org>
2023-05-25 14:56:20 +0530

    BG_Color_Chenge_Project

---
+++ color.js
@@ -0,0 +1,21 @@
+const btn1=document.getElementById("mainbox")
+const purple=document.getElementById("purplecolor")
+purple.onclick=function(){
+     btn1.style.backgroundColor="purple";
+}
+const blue=document.getElementById("bluecolor")
+blue.onclick=function(){
+     btn1.style.backgroundColor="blue";
+}
+const green=document.getElementById("greencolor")
+green.onclick=function(){
+     btn1.style.backgroundColor="green";
+}
+const red=document.getElementById("redcolor")
+red.onclick=function(){
+     btn1.style.backgroundColor="red";
+}
+const yellow=document.getElementById("yellowcolor")
+yellow.onclick=function(){
+     btn1.style.backgroundColor="yellow";
+}
